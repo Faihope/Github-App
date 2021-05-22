73132cda775672398239aefa161bdb14cc2a5eb4
Fai <chepngetichfaith46@gmail.com>
2021-05-22 21:05:56 +0300

    Add the result path to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { ResultsComponent } from './results/results.component';
 
 const routes: Routes = [
   {path:'form',component:FormComponent},
+  {path:'results',component:ResultsComponent},
+
   
 ];
 
