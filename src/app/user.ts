ff029c3688d7e4f17c4f40fabf8d23bc81bcced9
Fai <chepngetichfaith46@gmail.com>
2021-05-23 00:02:47 +0300

    add constructor and pass arguments to it in user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+        public bio:string,
+        public repos:string,
+        public login:string,
+        public avatar_url:String,
+        public created_at:Date
+    
+    ){
+
+    }
 }
