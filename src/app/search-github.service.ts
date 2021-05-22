6e77ed3fe67dcffc8f26ffac71963e39a58bb376
Fai <chepngetichfaith46@gmail.com>
2021-05-23 00:48:51 +0300

    add instance of class user

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { User } from "./user";
+import { Repository } from "./repository";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchGithubService {
+  user: User | undefined;
+  repository:Repository | undefined;
+  repodata=[];
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.user=new User("",0,"","", new Date)
+   }
 }
