8670af494b4e69d6bb471ab6c7b15c42db248f84
Fai <chepngetichfaith46@gmail.com>
2021-05-23 11:57:15 +0300

    Add the promise

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -8,10 +8,38 @@ import { Repository } from "./repository";
 })
 export class SearchGithubService {
   user: User | undefined;
-  repository:Repository | undefined;
-  repodata=[];
+  repository: Repository | undefined;
+  repodata = [];
 
-  constructor(private http:HttpClient) {
-    this.user=new User("",0,"","", new Date)
-   }
+  constructor(private http: HttpClient) {
+    this.user = new User("", 0, "", "", new Date)
+    this.repository = new Repository("", "", new Date, "", "")
+  }
+  getUser(username: string) {
+    interface ApiResponse {
+      bio: string,
+      public_repos: number,
+      login: string,
+      avatar_url: String,
+      created_at: Date
+    }
+    let promise =new Promise<void>((resolve,reject)=>{
+      this.http.get<ApiResponse>("https://api.github.com/users/" + username).toPromise().then(response =>{
+        this.user!.bio = response.bio;
+        this.user!.public_repos = response.public_repos;
+        this.user!.login = response.login;
+        this.user!.avatar_url = response.avatar_url;
+        this.user!. created_at = response. created_at;
+resolve()
+
+      },
+      error=>{
+        reject(error)
+      })
+      this.http.get<any>("https://api.github.com/users/" + username + "/repos").toPromise().then(response=>{
+        
+      })
+    })
+    
+  }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,7 @@
 export class User {
     constructor(
         public bio:string,
-        public repos:number,
+        public public_repos:number,
         public login:string,
         public avatar_url:String,
         public created_at:Date
